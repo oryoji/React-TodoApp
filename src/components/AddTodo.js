b9adb61c1ecca4af6db36d0d0e12a228fef5f1f2
oryoji <60314819+oryoji@users.noreply.github.com>
2020-12-02 20:06:36 +0900

    Add-AddTodo-Component

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -3,13 +3,29 @@ import React from "react";
 export class AddTodo extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { title: "" };
   }
 
   render() {
     return (
       <div>
         <h2>AddTodo</h2>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.title} onChange={this.handleChange} />
+          <input type="submit" value="Add todo list" />
+        </form>
       </div>
     );
   }
+
+  handleChange = (event) => {
+    const title = event.target.value;
+    this.setState({ title: title });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(this.state.title);
+    this.setState({ title: "" });
+  };
 }
